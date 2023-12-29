fe1b2b3221b180b3519a16a28558dcb2c3a8986b
Caro Maya Gonzalez <82158081+Caromaya@users.noreply.github.com>
2023-12-29 16:09:39 +0100

    Update add_sample_info.py
    Correction on 23,12,29 during meeting with Jesper

--- add_sample_info.py
+++ add_sample_info.py
@@ -5,37 +5,38 @@ first = True
 
 # Process the first file
 for line in open(sys.argv[1]):
-    content = line.strip().split(";")
+    content = line.strip().split(",")
     if first:
-        header = content[1:]
+        header = content[2:]
         first = False
         continue
-    data[content[0].split(" ")[0]] = content[1:]
+    #print(content[1].split(" ")[0])
+    #print(line.strip())
+    data[content[1].split(" ")[0]] = content[2:]
 
 first = True
 n_cols = len(header)
 
+#print(data)
+#quit()
+
+#if "GMSB-ST066" in data:
+#    print("carro!")
+
 # Process the second file
 for line in open(sys.argv[2]):
-    content = line.strip().split(";")
+    content = line.strip().split(",")
     if first:
-        print(line.strip() + ";" + ";".join(header))
+        print(line.strip() + "," + ",".join(header))
         first = False
         continue
+
+    #print(content[0])
+    #print(data[content[0]])
 
     content[0] = "-".join(content[0].replace("MIP", "").replace("-Research", "").rstrip("-").split("-")[0:2])
     
     try:
-        print(line.strip() + ";" + ";".join(data[content[0]]))
+        print(line.strip() + "," + ",".join(data[content[0]]))
     except KeyError:
-        # Key not found in data dictionary, check for specific mappings
-        if content[0] == "GMSB-066":
-            print(line.strip() + ";" + ";".join(data.get("GMSB-ST066", ["NA"] * n_cols)))
-        elif content[0] == "GMS-UP013":
-            print(line.strip() + ";" + ";".join(data.get("GMSB-UP013", ["NA"] * n_cols)))
-        elif content[0] == "GMS-UP011":
-            print(line.strip() + ";" + ";".join(data.get("GMSB-UP011", ["NA"] * n_cols)))
-        elif content[0] == "GMSB-UP0022":
-            print(line.strip() + ";" + ";".join(data.get("GMSB-UP022", ["NA"] * n_cols)))
-        else:
-            print(line.strip() + ";" + ";".join(["NA"] * n_cols))
+        print(line.strip() + "," + ",".join(["NA"] * n_cols))
